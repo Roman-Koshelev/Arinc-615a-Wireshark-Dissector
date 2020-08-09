77b5d0c148e42449104c00c0c9ae41c3bfea1a15
Roman Koshelev <kosh@redlab-i.ru>
2020-08-09 14:47:22 +0300

    1
    Signed-off-by: Roman Koshelev <kosh@redlab-i.ru>

--- packet-arinc615a.c
+++ packet-arinc615a.c
@@ -6,11 +6,6 @@
 
 #include <epan/dissectors/packet-tftp.h>
 
-#define PARS_RET_UINT8(proto_tree, name)                                                      \
-    guint32 name;                                                                             \
-    proto_tree_add_item_ret_uint(proto_tree, hf_a615a_##name, tvb, offset, 1, ENC_NA, &name); \
-    offset += 1
-
 #define PARS_RET_UINT16(proto_tree, name)                                                     \
     guint32 name;                                                                             \
     proto_tree_add_item_ret_uint(proto_tree, hf_a615a_##name, tvb, offset, 2, ENC_BIG_ENDIAN, \
@@ -25,29 +20,27 @@
     proto_tree_add_item(proto_tree, hf_a615a_##name, tvb, offset, 4, ENC_BIG_ENDIAN); \
     offset += 4
 
-#define PARS_OPERATION_STATUS_CODE(proto_tree, name)                                         \
+#define PARS_OPERATION_STATUS_CODE(proto_tree)                                               \
     do {                                                                                     \
-        PARS_RET_UINT16(proto_tree, name);                                                   \
-        col_append_fstr(pinfo->cinfo, COL_INFO, ", Status: %s",                              \
-                        val_to_str(name, a615a_operation_status_codes, "Unknown (0x%04x)")); \
+        PARS_RET_UINT16(proto_tree, operation_status);                                       \
+        col_append_fstr(                                                                     \
+            pinfo->cinfo, COL_INFO, ", Status: %s",                                          \
+            val_to_str(operation_status, a615a_operation_status_codes, "Unknown (0x%04x)")); \
     } while (0);
-
-#define PARS_STRING(proto_tree, name)                                                        \
-    proto_tree_add_item(proto_tree, hf_a615a_##name, tvb, offset, name##_length, ENC_ASCII); \
-    offset += name##_length
 
 #define PARS_LOAD_RATIO(proto_tree)                                                  \
     proto_tree_add_item(proto_tree, hf_a615a_load_ratio, tvb, offset, 3, ENC_ASCII); \
     offset += 3
 
-#define PARS_A615STRING_LENGTH(proto_tree, name) PARS_RET_UINT8(proto_tree, name##_length)
-
-#define PARS_A615STRING(proto_tree, name)         \
-    do {                                          \
-        PARS_A615STRING_LENGTH(proto_tree, name); \
-        if (name##_length > 0) {                  \
-            PARS_STRING(proto_tree, name);        \
-        }                                         \
+#define PARS_A615STRING(proto_tree, name)                                                 \
+    do {                                                                                  \
+        guint32 length;                                                                   \
+        proto_tree_add_item_ret_uint(proto_tree, hf_a615a_length, tvb, offset, 1, ENC_NA, \
+                                     &length);                                            \
+        offset += 1;                                                                      \
+        proto_tree_add_item(proto_tree, hf_a615a_##name, tvb, offset, length, ENC_ASCII); \
+        offset += length;                                                                 \
+                                                                                          \
     } while (0)
 
 enum A615A_SUFFIX { LCI, LCL, LCS, LNA, LND, LNL, LNO, LNR, LNS, LUI, LUR, LUS };
@@ -90,33 +83,22 @@ static gint ett_a615a = -1;
 static int hf_a615a_file_length = -1;
 static int hf_a615a_protocol_version = -1;
 static int hf_a615a_counter = -1;
-static int hf_a615a_info_operation_status = -1;
-static int hf_a615a_upload_operation_status = -1;
-static int hf_a615a_download_operation_status = -1;
-static int hf_a615a_part_load_operation_status = -1;
+static int hf_a615a_operation_status = -1;
 static int hf_a615a_exception_timer = -1;
 static int hf_a615a_estimated_time = -1;
-static int hf_a615a_status_description_length = -1;
+static int hf_a615a_length = -1;
 static int hf_a615a_status_description = -1;
 static int hf_a615a_load_ratio = -1;
 static int hf_a615a_file_count = -1;
-static int hf_a615a_file_name_length = -1;
 static int hf_a615a_file_name = -1;
-static int hf_a615a_file_description_length = -1;
 static int hf_a615a_file_description = -1;
-static int hf_a615a_part_number_length = -1;
 static int hf_a615a_part_number = -1;
 static int hf_a615a_number_target_hardware = -1;
-static int hf_a615a_literal_name_length = -1;
 static int hf_a615a_literal_name = -1;
-static int hf_a615a_serial_number_length = -1;
 static int hf_a615a_serial_number = -1;
 static int hf_a615a_part_number_count = -1;
-static int hf_a615a_ammendment_length = -1;
 static int hf_a615a_ammendment = -1;
-static int hf_a615a_designation_length = -1;
 static int hf_a615a_designation = -1;
-static int hf_a615a_user_data_length = -1;
 static int hf_a615a_user_data = -1;
 static int hf_a615a_file_type = -1;
 
@@ -147,7 +129,7 @@ static void dissect_a615a_LCL(tvbuff_t *tvb, packet_info *pinfo _U_, int offset,
 
 static void dissect_a615a_LUS(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *root)
 {
-    PARS_OPERATION_STATUS_CODE(root, upload_operation_status);
+    PARS_OPERATION_STATUS_CODE(root);
     PARS_A615STRING(root, status_description);
     PARS_UINT16(root, counter);
     PARS_UINT16(root, exception_timer);
@@ -165,7 +147,7 @@ static void dissect_a615a_LUS(tvbuff_t *tvb, packet_info *pinfo, int offset, pro
         PARS_A615STRING(part_root, file_name);
         PARS_A615STRING(part_root, part_number);
         PARS_LOAD_RATIO(part_root);
-        PARS_UINT16(part_root, part_load_operation_status);
+        PARS_UINT16(part_root, operation_status);
         PARS_A615STRING(part_root, status_description);
         proto_item_set_len(part_root, offset - begin_offset);
     }
@@ -174,7 +156,7 @@ static void dissect_a615a_LUS(tvbuff_t *tvb, packet_info *pinfo, int offset, pro
 static void dissect_a615a_LCS(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *root)
 {
     PARS_UINT16(root, counter);
-    PARS_OPERATION_STATUS_CODE(root, info_operation_status);
+    PARS_OPERATION_STATUS_CODE(root);
     PARS_UINT16(root, exception_timer);
     PARS_UINT16(root, estimated_time);
     PARS_A615STRING(root, status_description);
@@ -182,25 +164,25 @@ static void dissect_a615a_LCS(tvbuff_t *tvb, packet_info *pinfo, int offset, pro
 
 static void dissect_a615a_LUI(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *root)
 {
-    PARS_OPERATION_STATUS_CODE(root, upload_operation_status);
+    PARS_OPERATION_STATUS_CODE(root);
     PARS_A615STRING(root, status_description);
 }
 
 static void dissect_a615a_LCI(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *root)
 {
-    PARS_OPERATION_STATUS_CODE(root, info_operation_status);
+    PARS_OPERATION_STATUS_CODE(root);
     PARS_A615STRING(root, status_description);
 }
 
 static void dissect_a615a_LND(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *root)
 {
-    PARS_OPERATION_STATUS_CODE(root, download_operation_status);
+    PARS_OPERATION_STATUS_CODE(root);
     PARS_A615STRING(root, status_description);
 }
 
 static void dissect_a615a_LNO(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *root)
 {
-    PARS_OPERATION_STATUS_CODE(root, download_operation_status);
+    PARS_OPERATION_STATUS_CODE(root);
     PARS_A615STRING(root, status_description);
 }
 
@@ -254,15 +236,12 @@ static void dissect_a615a_LNR(tvbuff_t *tvb, packet_info *pinfo _U_, int offset,
         proto_item_set_len(part_root, offset - begin_offset);
     }
 
-    PARS_RET_UINT8(root, user_data_length);
-    if (user_data_length > 0) {
-        proto_tree_add_item(root, hf_a615a_user_data, tvb, offset, user_data_length, ENC_NA);
-    }
+    PARS_A615STRING(root, user_data);
 }
 
 static void dissect_a615a_LNS(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *root)
 {
-    PARS_OPERATION_STATUS_CODE(root, download_operation_status);
+    PARS_OPERATION_STATUS_CODE(root);
     PARS_A615STRING(root, status_description);
     PARS_UINT16(root, counter);
     PARS_UINT16(root, exception_timer);
@@ -279,7 +258,7 @@ static void dissect_a615a_LNS(tvbuff_t *tvb, packet_info *pinfo, int offset, pro
 
         int begin_offset = offset;
         PARS_A615STRING(part_root, file_name);
-        PARS_UINT16(part_root, download_operation_status);
+        PARS_UINT16(part_root, operation_status);
         PARS_A615STRING(part_root, file_description);
         proto_item_set_len(part_root, offset - begin_offset);
     }
@@ -402,88 +381,53 @@ void proto_register_a615a(void)
         {&hf_a615a_counter,
          {"Counter", "a615a.counter", FT_UINT16, BASE_DEC, NULL, 0x0, "A615a Protocol Counter",
           HFILL}},
-        {&hf_a615a_info_operation_status,
-         {"Info Operation Status Code", "a615a.info.status_code", FT_UINT16, BASE_DEC,
-          VALS(a615a_operation_status_codes), 0x0, "A615a Information Operation Status Code",
-          HFILL}},
+        {&hf_a615a_operation_status,
+         {"Status Code", "a615a.status_code", FT_UINT16, BASE_DEC,
+          VALS(a615a_operation_status_codes), 0x0, NULL, HFILL}},
         {&hf_a615a_exception_timer,
          {"Exception Timer", "a615a.exception_timer", FT_UINT16, BASE_DEC, NULL, 0x0,
           "A615a Exception Timer", HFILL}},
         {&hf_a615a_estimated_time,
          {"Estimated Time", "a615a.estimated_time", FT_UINT16, BASE_DEC, NULL, 0x0,
           "A615a Estimated Time", HFILL}},
-        {&hf_a615a_status_description_length,
-         {"Status Length", "a615a.status.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a Status Description Length", HFILL}},
+        {&hf_a615a_length,
+         {"Length", "a615a.len", FT_UINT8, BASE_DEC, NULL, 0x0, NULL, HFILL}},
         {&hf_a615a_status_description,
          {"Status Description", "a615a.status", FT_STRINGZ, BASE_NONE, NULL, 0x0,
           "A615a Status Description", HFILL}},
-        {&hf_a615a_upload_operation_status,
-         {"Upload Operation Status Code", "a615a.upload.status_code", FT_UINT16, BASE_DEC,
-          VALS(a615a_operation_status_codes), 0x0, "A615a Upload Operation Status Code", HFILL}},
-        {&hf_a615a_download_operation_status,
-         {"Download Operation Status Code", "a615a.download.status_code", FT_UINT16, BASE_DEC,
-          VALS(a615a_operation_status_codes), 0x0, "A615a Download Operation Status Code", HFILL}},
-        {&hf_a615a_part_load_operation_status,
-         {"Part Load Operation Status Code", "a615a.upload.status_code", FT_UINT16, BASE_DEC,
-          VALS(a615a_operation_status_codes), 0x0, "A615a Part Load Operation Status Code", HFILL}},
         {&hf_a615a_load_ratio,
          {"Load Ratio", "a615a.load_ratio", FT_STRINGZ, BASE_NONE, NULL, 0x0,
           "A615a Load Operation Ratio", HFILL}},
         {&hf_a615a_file_count,
          {"File Count", "a615a.file_count", FT_UINT16, BASE_DEC, NULL, 0x0, "A615a File Count",
           HFILL}},
-        {&hf_a615a_file_name_length,
-         {"File Name Length", "a615a.file_name.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a File Name Length", HFILL}},
         {&hf_a615a_file_name,
          {"File Name", "a615a.file_name", FT_STRINGZ, BASE_NONE, NULL, 0x0, "A615a File Name",
           HFILL}},
-        {&hf_a615a_file_description_length,
-         {"File Description Length", "a615a.file_description.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a File Description Length", HFILL}},
         {&hf_a615a_file_description,
          {"File Description", "a615a.file_description", FT_STRINGZ, BASE_NONE, NULL, 0x0,
           "A615a File Description", HFILL}},
-        {&hf_a615a_part_number_length,
-         {"Part Number Length", "a615a.part_number.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a Part Number Length", HFILL}},
         {&hf_a615a_part_number,
          {"Part Number", "a615a.part_number", FT_STRINGZ, BASE_NONE, NULL, 0x0, "A615a Part Number",
           HFILL}},
         {&hf_a615a_number_target_hardware,
          {"Number of Target Hardware", "a615a.num_hardware", FT_UINT16, BASE_DEC, NULL, 0x0,
           "A615a Number of Target Hardware", HFILL}},
-        {&hf_a615a_literal_name_length,
-         {"Literal Name Length", "a615a.literal_name.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a Literal Name Length", HFILL}},
         {&hf_a615a_literal_name,
          {"Literal Name", "a615a.literal_name", FT_STRINGZ, BASE_NONE, NULL, 0x0,
           "A615a Literal Name", HFILL}},
-        {&hf_a615a_serial_number_length,
-         {"Serial Number Length", "a615a.serial_number.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a Serial Number Length", HFILL}},
         {&hf_a615a_serial_number,
          {"Serial Number", "a615a.serial_number", FT_STRINGZ, BASE_NONE, NULL, 0x0,
           "A615a Serial Number", HFILL}},
         {&hf_a615a_part_number_count,
          {"Part Number Count", "a615a.num_parts", FT_UINT16, BASE_DEC, NULL, 0x0,
           "A615a Part Number Count", HFILL}},
-        {&hf_a615a_ammendment_length,
-         {"Ammendment Length", "a615a.ammendment.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a Ammendment Length", HFILL}},
         {&hf_a615a_ammendment,
          {"Ammendment", "a615a.ammendment", FT_STRINGZ, BASE_NONE, NULL, 0x0, "A615a Ammendment",
           HFILL}},
-        {&hf_a615a_designation_length,
-         {"Designation Length", "a615a.designation.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "A615a Designation Length", HFILL}},
         {&hf_a615a_designation,
          {"Designation", "a615a.designation", FT_STRINGZ, BASE_NONE, NULL, 0x0, "A615a Designation",
           HFILL}},
-        {&hf_a615a_user_data_length,
-         {"User Data Length", "a615a.user_data.length", FT_UINT8, BASE_DEC, NULL, 0x0,
-          "User Data Length", HFILL}},
         {&hf_a615a_user_data,
          {"User Data", "a615a.user_data", FT_BYTES, BASE_NONE, NULL, 0x0, "User Data", HFILL}},
         {&hf_a615a_file_type,
